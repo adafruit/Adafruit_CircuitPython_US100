15960b846b84144b0dc66042708e9a914bea63d5
Kattni Rembor <kattni@adafruit.com>
2019-11-13 12:54:23 -0500

    Added USB serial import to example

--- examples/us100_simpletest.py
+++ examples/us100_simpletest.py
@@ -9,6 +9,11 @@ uart = busio.UART(board.TX, board.RX, baudrate=9600)
 # For use with Raspberry Pi/Linux:
 # import serial
 # import adafruit_us100
+# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=1)
+
+# For use with Raspberry Pi/Linux:
+# import serial
+# import adafruit_us100
 # uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 us100 = adafruit_us100.US100(uart)
